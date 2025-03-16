1efc02d054acaf4d05c98b6fd2be8b226b8e0ade
Kilian-max <kgimenezb01@educarex.es>
2025-03-16 15:36:57 +0100

    Update index.php

--- proyecto_final/index.php
+++ proyecto_final/index.php
@@ -1,20 +1,21 @@
 <?php
 include("controller/productoController.php");
 
-// Punto de entrada a la aplicación. Si no se reciben los parámetros 'action' y 'controller'
-// se muestra la página de inicio con el listado de productos.
+//Punto de entrada a la aplicación. Si no se recibe parámetro action y controller en la url
+//se muestra la página de inicio (texto HTML).
+//En caso de que si se reciba, se instancia el controlador y se invoca la acción indicada.
 
 if (isset($_REQUEST['action']) && isset($_REQUEST['controller'])) {
     $act = $_REQUEST['action'];
     $cont = $_REQUEST['controller'];
 
-    // Instanciación del controlador e invocación del método
+    //Instanciación del controlador e invocación del método
     $controller = new $cont();
     $controller->$act();
 
 } 
 else {
-    // Página de entrada: Listado de productos por defecto
+    // Página de entrada: Llamamos a la funcion que lista los productos por defecto
     $controller = new ProductoController();
     $controller->listarProductos();
 }
