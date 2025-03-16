eb698cf7984924ea8f4b6f93eb23f87b976295aa
Kilian-max <kgimenezb01@educarex.es>
2025-03-16 15:43:45 +0100

    Delete proyecto_final directory

--- proyecto_final/views/finalizarCompra.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1 class=\"title is-1 has-text-centered has-text-success\">¡Compra finalizada con éxito!</h1>";
-echo "<p class=\"subtitle is-4 has-text-centered\">Gracias por tu compra</p>";
-?>
